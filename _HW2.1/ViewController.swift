ef31b2c6496dfe738dcfc12af10048f69f920afd
MedyakovDS <maxwellhouseq@yahoo.com>
2020-12-02 14:39:06 +0300

    FirstCommit

---
+++ _HW2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  _HW2.1
+//
+//  Created by Дмитрий Медяков on 02.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
